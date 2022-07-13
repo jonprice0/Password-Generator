6a10069c5d917d1738eb6535339c4b323a804d9d
Jonathan Price <jonprice0@gmail.com>
2022-07-13 17:17:38 -0400

    added main function, prompts and confirms with error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,25 @@ const arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 const arrayNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 const arraySymbol = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
 
-
+// initializing workingArray and password variables in generatePassword() function
+function generatePassword() {
+  var workingArray = []
+  var password = "";
+// prompts and confirms for user input with validation code  
+  var passwordLength = window.prompt("How many characters would you like your password to be?")
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Please choose a password length of at least 8 characters but no more than 128 characters.");
+      return "Try again!";
+    }
+    if (isNaN(passwordLength)) {
+      alert("Please enter a number.");
+      return "Try again!";
+    };
+  var lowerConfirm = confirm("Would you like your password to include lower-case letters?");
+  var upperConfirm = confirm("Would you like your password to include upper-case letters?");
+  var numberConfirm = confirm("Would you like your password to include numbers?");
+  var symbolConfirm = confirm("Would you like your password to include symbols?");
+};
 // This is the end of my code and the beginning of the starter code
 
 // Get references to the #generate element
