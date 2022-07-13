f01f57dbfee72a8fdc11f35132ef3cd40c387e65
Jonathan Price <jonprice0@gmail.com>
2022-07-13 16:25:24 -0400

    Defined the component character arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-// Assignment code here
+// initializing array components
+const arrayLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const arrayUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const arrayNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const arraySymbol = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
 
+
+// This is the end of my code and the beginning of the starter code
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
